fe56e844752ff8990b41892a5478db26716cbeac
Long Vu <longvh96@gmail.com>
2019-11-15 19:21:09 +0200

    Init project

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+import firestore from "firebase/firestore";
+
+const config = {
+  apiKey: process.env.firebaseApiKey,
+  authDomain: process.env.firebaseAuthDomain,
+  databaseURL: process.env.firebaseDatabaseURL,
+  projectId: process.env.firebaseProjectId,
+  storageBucket: process.env.firebaseStorageBucket,
+  messagingSenderId: process.env.firebaseMessagingSenderId
+}
+
+firebase.initializeApp(config);
+
+export default firebase;
